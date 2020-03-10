25905c89a8285564832bb28729791456e48585cf
Andy Rimmer <a.rimmer@elsevier.com>
2020-03-10 16:32:26 +0000

    First version of PDF generation and conversion to HTML5

--- generate_pdf.py
+++ generate_pdf.py
@@ -1,8 +1,17 @@
 from pylatex import Document
 
 
+def remove_file_extension(file_name: str) -> str:
+    return file_name.split(".")[0]
+
+
 def generate_pdf_file_with_simple_content(file_name: str, content: str) -> None:
-    pdf_file = Document(file_name)
+
+    pdf_file = Document(
+        default_filepath=remove_file_extension(file_name),
+        page_numbers=False
+    )
+
     pdf_file.append(content)
     pdf_file.generate_tex()
     pdf_file.generate_pdf(clean_tex=False)
--- normalise_pdf.py
+++ normalise_pdf.py
@@ -1,5 +1,6 @@
-import html
-
+import dominate
+from dominate.tags import *
+from dominate.util import raw
 from pdfminer.high_level import extract_text
 
 
@@ -8,4 +9,43 @@ def extract_text_from_pdf(pdf_file_name):
 
 
 def generate_normalised_html_from_pdf_file(pdf_file_name):
+    """
+    <!DOCTYPE html>
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:base="https://example.com/minimal">
+      <head>
+        <link rel="schema.dct" href="http://purl.org/dc/terms/" />
+        <meta name="id" content="https://example.com/minimal"></meta>
+        <meta name="dct:conformsTo" content="http://data.elsevier.com/schema/led/" />
+        <script type="application/ld+json">
+          {
+            "@context": ["https://data.elsevier.com/schema/led/", {"doc": "https://example.com/minimal#"}],
+            "@id": "https://example.com/minimal",
+            "dct:conformsTo": ["https://data.elsevier.com/schema/led/", "https://data.elsevier.com/schema/edm/"]
+          }
+        </script>
+      </head>
+      <body>
+      </body>
+    </html>
+    """
     text_content = extract_text_from_pdf(pdf_file_name)
+    print(text_content)
+    html_doc = dominate.document(title="")
+
+    with html_doc.head:
+        link(rel="schema.dct", href="http://purl.org/dc/terms/")
+        meta(name="id", content="https://example.com/minimal")
+        meta(name="dct:conformsTo", content="http://data.elsevier.com/schema/led/")
+        script(
+            type="application/ld+json",
+            text_content=raw("""{
+            "@context": ["https://data.elsevier.com/schema/led/", {"doc": "https://example.com/minimal#"}],
+            "@id": "https://example.com/minimal",
+            "dct:conformsTo": ["https://data.elsevier.com/schema/led/", "https://data.elsevier.com/schema/edm/"]
+          }""")
+        )
+
+    with html_doc:
+        section(text_content)
+
+    return html_doc.render().replace("<title></title>\n", "")  # Hack to remove unwanted title
